af91fa9dd35fe701df4ff638bee4f25ce020366d
rahulyadav02996 <77198600+Rahulyadav0296@users.noreply.github.com>
2024-05-28 11:50:07 +0530

    First Commit

---
+++ components/images/image-slideshow.js
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+import Image from "next/image";
+
+import burgerImg from "@/assets/burger.jpg";
+import curryImg from "@/assets/curry.jpg";
+import dumplingsImg from "@/assets/dumplings.jpg";
+import macncheeseImg from "@/assets/macncheese.jpg";
+import pizzaImg from "@/assets/pizza.jpg";
+import schnitzelImg from "@/assets/schnitzel.jpg";
+import tomatoSaladImg from "@/assets/tomato-salad.jpg";
+import classes from "./image-slideshow.module.css";
+
+const images = [
+  { image: burgerImg, alt: "A delicious, juicy burger" },
+  { image: curryImg, alt: "A delicious, spicy curry" },
+  { image: dumplingsImg, alt: "Steamed dumplings" },
+  { image: macncheeseImg, alt: "Mac and cheese" },
+  { image: pizzaImg, alt: "A delicious pizza" },
+  { image: schnitzelImg, alt: "A delicious schnitzel" },
+  { image: tomatoSaladImg, alt: "A delicious tomato salad" },
+];
+
+export default function ImageSlideshow() {
+  // manage the behaviour of the state in the component
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  // useEffect is usefull for removing the side effects problem like when we re-render the component so it will render one time because we set the array empty
+  // which show that there is no dependencies we have
+  // we calling the pure function here which tells that same input returns the same output
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex < images.length - 1 ? prevIndex + 1 : 0
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={classes.slideshow}>
+      {images.map((image, index) => (
+        <Image
+          key={index}
+          src={image.image}
+          className={index === currentImageIndex ? classes.active : ""}
+          alt={image.alt}
+        />
+      ))}
+    </div>
+  );
+}
---
+++ components/main-header/main-header.js
@@ -0,0 +1,35 @@
+"use client"; // this identifies that we are using client side renderring because NextJs provide server side rendering
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "./main-header-background";
+import NavLink from "./nav-link";
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image
+            src={logoImg}
+            alt="An Logo of Nexts Food Application"
+            priority
+          />
+          NextLevel Food
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <NavLink href="/meals"> Browse the Meals </NavLink>
+            </li>
+            <li>
+              <NavLink href="/community"> Foodie Community </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
